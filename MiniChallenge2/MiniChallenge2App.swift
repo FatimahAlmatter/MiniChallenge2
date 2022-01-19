34e843f841575bb202703f2900fbc6856cbe711f
Fatimah <f-y.1416@hotmail.com>
2022-01-19 17:22:48 +0300

    Initial Commit

---
+++ MiniChallenge2/MiniChallenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge2App.swift
+//  MiniChallenge2
+//
+//  Created by Fatimah Almatter on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
