ea8cc041a4cfaa006c2134038da1938ae24c782f
Fatimah <f-y.1416@hotmail.com>
2022-01-19 22:28:16 +0300

    second commit

--- MiniChallenge2/Projects.swift
+++ MiniChallenge2/Projects.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct Projects: View {
+    @State var allProjects = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ScrollView{
+                VStack{
+                    //Create Segmented Control
+                    Picker(selection: $allProjects) {
+                        Text("All").tag(0)
+                        Text("Pending").tag(1)
+                        Text("In Progress").tag(2)
+                        Text("Done").tag(3)
+                        
+                    } label: {
+                        Text("Projects")
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                }
+                .padding()
+            }
+            .navigationTitle("Projects")
+        }
     }
 }
 
