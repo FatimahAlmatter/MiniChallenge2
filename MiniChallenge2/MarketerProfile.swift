fb1f7fcd1e2a93e1ffb02a4bb82e302bbcd22bf8
shoogalmalki <shoogalmalki11@gmail.com>
2022-01-20 12:57:03 +0300

    SHOOG COMMAIT

--- MiniChallenge2/MarketerProfile.swift
+++ MiniChallenge2/MarketerProfile.swift
@@ -13,6 +13,8 @@ struct MarketerProfile: View {
         ZStack{
             Rectangle()
                 .fill(Color.white)
+                .padding(.leading, -16.0)
+            
                 .frame(width: 356, height: 357)
             VStack{
                 Image( "Bac")
@@ -24,19 +26,70 @@ struct MarketerProfile: View {
                 Text("Marketer Major")
                     .font(.custom("SF Pro Rounded", size: 14))
                     .fontWeight(.medium)
-              Image(systemName: "star.fill")
-                    .foregroundColor(Color("Yellow"))
+                HStack{
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color("Yellow"))
+                        .padding([.bottom], 3.0)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color("Yellow"))
+                        .padding(.bottom, 3.0)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color("Yellow"))
+                        .padding(.bottom, 3.0)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color("Yellow"))
+                        .padding(.bottom, 3.0)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(Color("LightGrey"))
+                        .padding(.bottom, 3.0)
+                    
+                }
+                Divider()
+                    .padding(.bottom, -33.0)
+                
+                HStack{
+                    
+                    Text("Portfolio Link:")
+                        .font(.custom("SF Pro Rounded", size: 16))
+                        .fontWeight(.medium)
+                        .padding(.leading, -100.0)
+                    Link(destination: URL(string:"www.my-portfolio.com")!) {
+                        Text("www.my-portfolio.com")
+                            .font(.custom("SF Pro Rounded", size: 16))
+                            .fontWeight(.medium)
+                            .foregroundColor(Color("blue"))
+                        
+                            .underline()
+                        
+                    }
+                    
+                    
+                    
+                }
+                Text("Completed Projects: 13")
+                    .font(.custom("SF Pro Rounded", size: 16))
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.leading)
+                    .padding(.leading, -183.0)
+                
+                Text("Skills: video editing, photoshop, drawing")
+                    .font(.custom("SF Pro Rounded", size: 16))
+                    .padding(.leading, -84.0)
             }
+            
+            
+            //            .padding(.horizontal, 21.0)
+            
         }
-       
-        
     }
+    
+    
 }
-
-
-
 struct MarketerProfile_Previews: PreviewProvider {
     static var previews: some View {
         MarketerProfile()
+            .padding(.leading)
     }
 }
+
