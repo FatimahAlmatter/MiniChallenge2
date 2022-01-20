60943c71ed7fc316234752ca2853a95987154849
Fatimah <f-y.1416@hotmail.com>
2022-01-20 12:50:21 +0300

    Fatimah's commit

--- MiniChallenge2/Projects.swift
+++ MiniChallenge2/Projects.swift
@@ -12,25 +12,26 @@ struct Projects: View {
     var body: some View {
         NavigationView{
             ScrollView{
-                VStack{
-                    //Create Segmented Control
-                    Picker(selection: $allProjects) {
-                        Text("All").tag(0)
-                        Text("Pending").tag(1)
-                        Text("In Progress").tag(2)
-                        Text("Done").tag(3)
-                        
-                    } label: {
-                        Text("Projects")
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
+                //Create Segmented Control
+                Picker(selection: $allProjects) {
+                    Text("All").tag(0)
+                    Text("Pending").tag(1)
+                    Text("In Progress").tag(2)
+                    Text("Done").tag(3)
+                    
+                } label: {
+                    Text("Projects")
                 }
+                .pickerStyle(SegmentedPickerStyle())
                 .padding()
                 
+                //card 1
                 VStack(alignment: .leading, spacing: 10){
                     Text("Ad for Sunglasses Store")
+                        .font(.custom("SF Pro Rounded", size: 20))
                         .fontWeight(.bold)
                         .foregroundColor(Color("DarkGrey"))
+
                     Text("Date Posted: 20/01/2022")
                         .fontWeight(.medium)
                         .foregroundColor(Color("LightGrey"))
@@ -40,6 +41,32 @@ struct Projects: View {
                         .frame(width: 66, height: 20)
                         .background(Color("Yellow"))
                         .cornerRadius(13)
+                        .padding(.bottom,20)
+                    
+                    HStack(alignment: .center){
+                        
+                        
+                        NavigationLink {
+                            ProjectDetails()
+                        } label: {
+                            Text("View Details")
+                                .foregroundColor(.white)
+                                .frame(width: 160, height: 40)
+                                .background(Color("AccentColor"))
+                                .cornerRadius(12)
+                        }
+                        
+                        NavigationLink {
+                            Offers()
+                        } label: {
+                            Text("Offers")
+                                .foregroundColor(.white)
+                                .frame(width: 160, height: 40)
+                                .background(Color("AccentColor"))
+                                .cornerRadius(12)
+                        }
+
+                    }
                     
                     
                 }
@@ -48,12 +75,13 @@ struct Projects: View {
                 .background(Color("CardsColor"))
                 .cornerRadius(12)
                 
-                
             }
             .navigationTitle("Projects")
         }
+        
     }
 }
+
 
 struct Projects_Previews: PreviewProvider {
     static var previews: some View {
