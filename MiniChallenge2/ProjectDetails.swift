330a1caca4a774441d291e5a7f2f0f0a670bba78
Fatimah <f-y.1416@hotmail.com>
2022-01-20 12:58:26 +0300

    last changes

--- MiniChallenge2/ProjectDetails.swift
+++ MiniChallenge2/ProjectDetails.swift
@@ -2,72 +2,14 @@
 //  ProjectDetails.swift
 //  MiniChallenge2
 //
-//  Created by Fatimah Almatter on 19/01/2022.
+//  Created by Fatimah Almatter on 20/01/2022.
 //
 
 import SwiftUI
 
 struct ProjectDetails: View {
-    @State var allProjects = 0
     var body: some View {
-        NavigationView{
-            ScrollView{
-                
-                VStack{
-                    //Create Segmented Control
-                    Picker(selection: $allProjects) {
-                        Text("All").tag(0)
-                        Text("Recent").tag(1)
-                        
-                    } label: {
-                        Text("Projects")
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
-                .padding()
-                ZStack{
-                    VStack{
-                        
-                        HStack{
-                            Image("marketer1")
-                                .resizable()
-                                .frame(width: 60, height: 60)
-                                .clipShape(Circle())
-                            
-                            Text("AbhoyLatif")
-                                .font(.custom("SF Pro Rounded", size: 14))
-                                .fontWeight(.medium)
-                            
-                        }
-                        HStack{
-                            Image(systemName: "star.fill")
-                                .foregroundColor(Color("Yellow"))
-                                .padding([.leading, .bottom], 172.0)
-                            Image(systemName: "star.fill")
-                                .foregroundColor(Color("Yellow"))
-                                .padding(.bottom, 172.0)
-                            Image(systemName: "star.fill")
-                                .foregroundColor(Color("Yellow"))
-                                .padding(.bottom, 172.0)
-                            Image(systemName: "star.fill")
-                                .foregroundColor(Color("Yellow"))
-                                .padding(.bottom, 172.0)
-                            Image(systemName: "star.fill")
-                                .foregroundColor(Color("LightGrey"))
-                                .padding(.bottom, 172.0)
-                            
-                            
-                        }
-                        
-                        
-                    }
-                    
-                }
-                .navigationTitle("offers")
-                
-            }
-        }
-        
+        Text("ProjectDetails")
     }
 }
 
@@ -76,4 +18,3 @@ struct ProjectDetails_Previews: PreviewProvider {
         ProjectDetails()
     }
 }
-
