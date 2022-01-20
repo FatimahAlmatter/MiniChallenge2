f900818d99610fdb88060a53aa05285685b3d514
Mahaaldajanii <maha050002@gmail.com>
2022-01-20 13:16:43 +0300

    1

--- MiniChallenge2/Offers.swift
+++ MiniChallenge2/Offers.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct Offers: View {
     @State var allProjects = 0
     var body: some View {
-        NavigationView{
+        
             ScrollView{
                 
                 VStack{
@@ -26,47 +26,94 @@ struct Offers: View {
                 }
                 .padding()
                 ZStack{
-                    VStack{
+                    VStack(alignment: .leading){
                         
                         HStack{
                             Image("marketer1")
                                 .resizable()
+                                .padding(.trailing, 1.0)
                                 .frame(width: 60, height: 60)
                                 .clipShape(Circle())
                             
                             Text("AbhoyLatif")
-                                .font(.custom("SF Pro Rounded", size: 14))
-                                .fontWeight(.medium)
+                                .font(.custom("SF Pro Rounded", size: 19))
+                                .fontWeight(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(Color("DarkGrey"))
+                                .multilineTextAlignment(.leading)
+                                .padding(.trailing, 31.0)
+                                .frame(width: 140.0)
+                                
+                            Image(systemName: "message.circle.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                            .foregroundColor(Color("blue"))
+                            .padding(.leading, 80.0)
+                            .padding(.bottom, 1.0)
                             
                         }
+                        
                         HStack{
                             Image(systemName: "star.fill")
+                                .resizable()
+                                .frame(width: 15, height: 15)
                                 .foregroundColor(Color("Yellow"))
-                                .padding([.leading, .bottom], 172.0)
+                            
+                                .padding(.bottom, 190.0)
+                                .padding(.leading, 80.0)
+                                
                             Image(systemName: "star.fill")
+                                .resizable()
+                                .frame(width: 15, height: 15)
                                 .foregroundColor(Color("Yellow"))
-                                .padding(.bottom, 172.0)
+                            .padding(.bottom, 190.0 )
+                                .padding(.leading, 3.0)
+                            
                             Image(systemName: "star.fill")
+                                .resizable()
+                                .frame(width: 15, height: 15)
                                 .foregroundColor(Color("Yellow"))
-                                .padding(.bottom, 172.0)
+                                .padding(.bottom, 190.0)
                             Image(systemName: "star.fill")
+                                .resizable()
+                                .frame(width: 15, height: 15)
                                 .foregroundColor(Color("Yellow"))
-                                .padding(.bottom, 172.0)
+                                .padding(.bottom, 190.0)
                             Image(systemName: "star.fill")
+                                .resizable()
+                                .frame(width: 15, height: 15)
                                 .foregroundColor(Color("LightGrey"))
-                                .padding(.bottom, 172.0)
+                                .padding(.bottom, 190.0)
                             
                             
                         }
+                       
+                        .padding(.trailing, 0.0)
                         
-                        
+                     
+
+
+                    }
+                    VStack {
+                        Text("")
+                        Divider()
+                        Text("")
+                       
                     }
                     
+                    .padding([.bottom, .trailing], 42.0)
+
+
                 }
-                .navigationTitle("offers")
-                
+                .frame(width: 356, height: 160)
+                .padding([.top, .trailing], 59.0)
+                .background(Color("CardsColor"))
+                .cornerRadius(12)
             }
-        }
+                .navigationTitle("offers")
+                .navigationBarTitleDisplayMode(.large)
+                
+            
+        
     }
 }
 
