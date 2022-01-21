86c225429ef496a9e83648642ec9f45c0ba46a10
FatimahAlmatter <f-y.1416@hotmail.com>
2022-01-21 22:29:43 +0300

    Final Changes for Friday

--- MiniChallenge2/Onboarding.swift
+++ MiniChallenge2/Onboarding.swift
@@ -9,12 +9,45 @@ import SwiftUI
 
 struct Onboarding: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Circle()
+                .frame(width: 290, height: 290, alignment: .center)
+                .foregroundColor(Color.accentColor)
+                .padding(.bottom,155)
+            
+            Rectangle()
+                .edgesIgnoringSafeArea(.bottom)
+                .foregroundStyle(.ultraThinMaterial)
+                .frame(width: 390, height: 470, alignment: .center)
+                .padding(.top,310)
+            
+            Text("With CREAT Find Your Creative Marketer Faster")
+                .tracking(6.41)
+                .padding(.top,100)
+                .foregroundColor(Color("DarkGrey"))
+                .font(Font.custom("SF Compact", size: 30))
+                .lineSpacing(6)
+            
+            
+            NavigationLink {
+                Projects()
+            } label: {
+                Text("START")
+                    .font(Font.custom("SF Compact", size: 15))
+                    .foregroundColor(.white)
+                    .frame(width: 160, height: 40)
+                    .background(Color("AccentColor"))
+                    .cornerRadius(12)
+            }
+            .padding(.top,560)
+            
+        }
     }
 }
 
 struct Onboarding_Previews: PreviewProvider {
     static var previews: some View {
         Onboarding()
+            .preferredColorScheme(.light)
     }
 }
