7ecb1ae9100a8cafbf982fb8d41ffe6f34aa5945
Shahad Benmohi <96819901+ShahadMBM@users.noreply.github.com>
2022-01-21 17:53:01 +0300

    added some features for  (Post,Details project) screen

--- MiniChallenge2/PostProject.swift
+++ MiniChallenge2/PostProject.swift
@@ -8,10 +8,108 @@
 import SwiftUI
 
 struct PostProject: View {
+    @State  var textName: String = ""
+    @State  var textDes: String = ""
+    @State  var textOther: String = ""
+    @State  var textBudget: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+            NavigationView{
+                
+            ScrollView{
+                
+            VStack{
+                
+                
+                VStack(alignment: .leading){
+                    
+                Text("Project Name")
+                    .fontWeight(.bold)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color("LightGrey"))
+                    
+                TextField("Enter project name ", text: $textName )
+                  
+                    
+                
+             Text("Project Description")
+                    .fontWeight(.bold)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color("LightGrey"))
+                TextField("Enter project description", text: $textDes)
+                
+                      
+             Text("Time\n") // calander tab
+                    .fontWeight(.bold)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color("LightGrey"))
+                
+              Text("Category\n") // drop-down lis
+                    .fontWeight(.bold)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color("LightGrey"))
+                    
+                
+                    Group{
+                        
+               Text("Others")
+                    .fontWeight(.bold)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color("LightGrey"))
+                
+                TextField(".. ", text: $textOther)
+                
+                   Text("Upload Files")
+                    .fontWeight(.bold)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color("LightGrey"))
+                
+               Text("Budget")
+                    .fontWeight(.bold)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color("LightGrey"))
+                
+            TextField("1000 - 2000", text: $textBudget)
+                        
+                    }
+            }
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                
+                Spacer()
+                
+                HStack{
+            
+                    Button(action: {}) {
+                            Text("Post")
+                                .fontWeight(.medium)
+                                .foregroundColor(.white)
+                                 
+                    } .frame(width: 160, height: 40)
+                        .background(Color("AccentColor"))
+                        .cornerRadius(12)
+                
+                    
+                    Button(action: {} ) {
+                        Text("Cancel")
+                            .fontWeight(.medium)
+                            .foregroundColor(Color("AccentColor"))
+                    }
+                    .frame(width: 160, height: 40)
+                        .background(Color.white)
+                        .cornerRadius(12)
+                }
+            }
+                }
+            .navigationTitle("Post Project")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        }
+    
 }
+
 
 struct PostProject_Previews: PreviewProvider {
     static var previews: some View {
--- MiniChallenge2/ProjectDetails.swift
+++ MiniChallenge2/ProjectDetails.swift
@@ -8,10 +8,76 @@
 import SwiftUI
 
 struct ProjectDetails: View {
+    @State  var textNameD: String = ""
+    @State  var textDesD: String = ""
+    @State  var textOtherD: String = ""
+    @State  var textBudgetD: String = ""
     var body: some View {
-        Text("ProjectDetails")
+        NavigationView{
+            
+            
+            VStack(alignment: .leading){
+                
+            Text("Project Name")
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .foregroundColor(Color("LightGrey"))
+                
+            TextField("Enter project name ", text: $textNameD )
+              
+                
+            
+         Text("Project Description")
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .foregroundColor(Color("LightGrey"))
+            TextField("Enter project description", text: $textDesD)
+            
+                  
+         Text("Time\n") // calander tab
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .foregroundColor(Color("LightGrey"))
+            
+          Text("Category\n") // drop-down lis
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .foregroundColor(Color("LightGrey"))
+                
+            
+                Group{
+                    
+           Text("Others")
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .foregroundColor(Color("LightGrey"))
+            
+            TextField(".. ", text: $textOtherD)
+            
+               Text("Upload Files")
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .foregroundColor(Color("LightGrey"))
+            
+           Text("Budget")
+                .fontWeight(.bold)
+                .font(.system(size: 16))
+                .foregroundColor(Color("LightGrey"))
+            
+        TextField("1000 - 2000", text: $textBudgetD)
+                    
+                }
+        }
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+    .navigationTitle("Project Details")
+    .navigationBarTitleDisplayMode(.inline)
+        }
+    
     }
 }
+
 
 struct ProjectDetails_Previews: PreviewProvider {
     static var previews: some View {
