4da4e4099d2abe094991aa554fb346a1debc10f2
FatimahAlmatter <f-y.1416@hotmail.com>
2022-01-22 22:52:59 +0300

    final commit for saturday

--- MiniChallenge2/Notifications.swift
+++ MiniChallenge2/Notifications.swift
@@ -11,17 +11,36 @@ struct Notifications: View {
     
     var body: some View {
         NavigationView{
-            Text("Hello, World!")
-            
-            
-                .navigationTitle("Notifications")
-                .toolbar(content: {
-                    Text("Clear All")
-                        .font(.title3)
-                        .foregroundColor(Color.accentColor)
-                })
+            ScrollView{
+                notification(title: "New offer proposed for Ad for Sunglasses Store")
+                notification(title: "New offer proposed for Online Service Ad")
+                notification(title: "Ad for Perfume status changed to Done")
+                
+            }
+            .navigationTitle("Notifications")
+            .toolbar(content: {
+                Text("Clear All")
+                    .foregroundColor(Color.accentColor)
+            })
             
         }
+    }
+    func notification(title:String) -> some View{
+        return HStack{
+            Text(title)
+                .font(.custom("SF Compact Rounded Medium", size: 17))
+                .foregroundColor(Color("DarkGrey"))
+                .lineLimit(nil)
+            
+            Spacer()
+            Image(systemName: "multiply.circle.fill")
+                .foregroundColor(Color.accentColor)
+                .padding(.bottom,25)
+        }
+        .padding()
+        .frame(width: 356, alignment: .leading)
+        .background(Color("CardsColor"))
+        .cornerRadius(12)
     }
 }
 
