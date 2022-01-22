61718abea30a83f9678c18e3749fcc0da3897f27
Shahad Benmohi <96819901+ShahadMBM@users.noreply.github.com>
2022-01-23 00:42:54 +0300

    ...

--- MiniChallenge2/PostProject.swift
+++ MiniChallenge2/PostProject.swift
@@ -12,97 +12,121 @@ struct PostProject: View {
     @State  var textDes: String = ""
     @State  var textOther: String = ""
     @State  var textBudget: String = ""
+    @State  var selectionDate: Date = Date()
     
     var body: some View {
         
-            NavigationView{
-                
+        NavigationView{
+            
             ScrollView{
                 
-            VStack{
-                
-                
-                VStack(alignment: .leading){
+                VStack{
                     
-                Text("Project Name")
-                    .fontWeight(.bold)
-                    .font(.system(size: 16))
-                    .foregroundColor(Color("LightGrey"))
                     
-                TextField("Enter project name ", text: $textName )
-                  
-                    
-                
-             Text("Project Description")
-                    .fontWeight(.bold)
-                    .font(.system(size: 16))
-                    .foregroundColor(Color("LightGrey"))
-                TextField("Enter project description", text: $textDes)
-                
-                      
-             Text("Time\n") // calander tab
-                    .fontWeight(.bold)
-                    .font(.system(size: 16))
-                    .foregroundColor(Color("LightGrey"))
-                
-              Text("Category\n") // drop-down lis
-                    .fontWeight(.bold)
-                    .font(.system(size: 16))
-                    .foregroundColor(Color("LightGrey"))
-                    
-                
-                    Group{
+                    VStack(alignment: .leading){
                         
-               Text("Others")
-                    .fontWeight(.bold)
-                    .font(.system(size: 16))
-                    .foregroundColor(Color("LightGrey"))
-                
-                TextField(".. ", text: $textOther)
-                
-                   Text("Upload Files")
-                    .fontWeight(.bold)
-                    .font(.system(size: 16))
-                    .foregroundColor(Color("LightGrey"))
-                
-               Text("Budget")
-                    .fontWeight(.bold)
-                    .font(.system(size: 16))
-                    .foregroundColor(Color("LightGrey"))
-                
-            TextField("1000 - 2000", text: $textBudget)
+                        Text("Project Name")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                            .foregroundColor(Color("LightGrey"))
+                        
+                        TextField("Enter project name ", text: $textName )
+                        
+                        Spacer()
+                        
+                        Text("Project Description")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                            .foregroundColor(Color("LightGrey"))
+                        TextField("Enter project description", text: $textDes)
+                        
+                        Spacer()
+                        
+                        Group{
+                            //
+                            
+                            DatePicker("Time", selection: $selectionDate)
+                                .foregroundColor(Color("LightGrey"))
+                                .font(.system(size: 16))
+                                
+                            
+                              
+                            
+                            Text("Category\n") // drop-down lis
+                                .fontWeight(.bold)
+                                .font(.system(size: 16))
+                                .foregroundColor(Color("LightGrey"))
+                            
+                            
+                            Text("Others")
+                                .fontWeight(.bold)
+                                .font(.system(size: 16))
+                                .foregroundColor(Color("LightGrey"))
+                            
+                            TextField(".. ", text: $textOther)
+                        
+                        
+                        Text("Upload Files")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                            .foregroundColor(Color("LightGrey"))
+                        
+                        ZStack{
+                            
+                            TextField("Files", text: .constant(""))
+                            
+                        }
+                        }
+                        Text("Budget")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                            .foregroundColor(Color("LightGrey"))
+                        
+                        TextField("Ex : 1000 - 2000", text: $textBudget)
+                        
                         
                     }
-            }
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                
-                
-                Spacer()
-                
-                HStack{
-            
-                    Button(action: {}) {
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    
+                    Spacer()
+                    
+                    HStack{
+                        
+                        NavigationLink{
+                            Projects()
+                        }label: {
                             Text("Post")
                                 .fontWeight(.medium)
                                 .foregroundColor(.white)
-                                 
-                    } .frame(width: 160, height: 40)
-                        .background(Color("AccentColor"))
-                        .cornerRadius(12)
-                
-                    
-                    Button(action: {} ) {
-                        Text("Cancel")
-                            .fontWeight(.medium)
-                            .foregroundColor(Color("AccentColor"))
-                    }
-                    .frame(width: 160, height: 40)
+                            
+                        } .frame(width: 160, height: 40)
+                            .background(Color("AccentColor"))
+                            .cornerRadius(12)
+                        
+                        
+                        NavigationLink {
+                            Projects()
+                        } label: {
+                            Text("Cancel")
+                                .fontWeight(.medium)
+                                .foregroundColor(Color("AccentColor"))
+                            
+                        }
+                        .frame(width: 160, height: 40)
                         .background(Color.white)
                         .cornerRadius(12)
+                        //// لسا بيتعدل
+                        .border(Color("AccentColor"), width: 0.5)
+                       
+
+                        
+                        
+                    }
+                                        
                 }
             }
-                }
             .navigationTitle("Post Project")
             .navigationBarTitleDisplayMode(.inline)
         }
