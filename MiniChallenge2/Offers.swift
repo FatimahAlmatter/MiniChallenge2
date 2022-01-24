3900212f53e7fd7e871f01d738da1bd9997214ca
Mahaaldajanii <maha050002@gmail.com>
2022-01-24 12:51:06 +0300

    jok

--- MiniChallenge2/Offers.swift
+++ MiniChallenge2/Offers.swift
@@ -171,8 +171,6 @@ struct Offers: View {
                               )
                         }
                    
-                     
-                        
                     }
                     
                     
