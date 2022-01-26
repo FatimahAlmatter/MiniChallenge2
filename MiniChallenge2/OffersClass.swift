cb2607a888de4562db8e0b08fdc0cc7873c48da5
FatimahAlmatter <f-y.1416@hotmail.com>
2022-01-26 20:54:49 +0300

    Last commit

--- MiniChallenge2/OffersClass.swift
+++ MiniChallenge2/OffersClass.swift
@@ -36,7 +36,7 @@ class OffersClass : ObservableObject{
         offers(id: 1, marketerName: "Khaled Ali", priceoffers: "100$", timeoffers: "2 Weeks", marketerImage: "marketer1", projectID: 1, offerStatus: "Accepted", marketerID: 1),
         offers(id: 2, marketerName: "Nora faisal", priceoffers: "236$", timeoffers: "1 months", marketerImage: "marketer2", projectID: 1, offerStatus: "Non", marketerID: 2),
         offers(id: 3, marketerName: "Sara", priceoffers: "125$", timeoffers: "2 months", marketerImage: "marketer3", projectID: 2, offerStatus: "Non", marketerID: 3),
-        offers(id: 4, marketerName: "Fahad Bader", priceoffers: "500$", timeoffers: "4 Days", marketerImage: "marketer3", projectID: 3, offerStatus: "Non", marketerID: 4)
+        offers(id: 4, marketerName: "Fahad Bader", priceoffers: "500$", timeoffers: "4 Days", marketerImage: "img2", projectID: 3, offerStatus: "Non", marketerID: 4)
     ]
     
     @Published var marketer : [marketer] = [
